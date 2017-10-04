4676e334e12610a00973305cf1d7c8696d30a33b
Axel Olsson <axel.olsson@chasacademy.se>
2017-10-04 08:38:43 +0200

    Initial structure

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Globala variabler
+
+var wordList; // Lista med spelets alla ord
+var selectedWord; // Ett av orden valt av en slumpgenerator
+var letterBoxes; //Rutorna där bokstäverna ska stå
+var hangmanImg; //Bild som kommer vid fel svar
+var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
+var msgElem; // Ger meddelande när spelet är över
+var startGameBtn; // Knappen du startar spelet med
+var letterButtons; // Knapparna för bokstäverna
+var startTime; // Mäter tiden
+
+// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
+// Initiering av globala variabler samt koppling av funktioner till knapparna.
+function init() {} // End init
+
+window.onload = init; // Se till att init aktiveras då sidan är inladdad
+
+// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+
+// Funktion som slumpar fram ett ord
+ 
+// Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
+
+// Funktion som körs när du trycker på bokstäverna och gissar bokstav
+
+// Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
+
+// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
